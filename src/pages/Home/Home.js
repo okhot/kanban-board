cbbd2d6112f94b57aaf26c9591b4f99ce8365686
Mandela <arreyetta1jnr@gmail.com>
2022-11-05 22:11:59 +0100

    Complete card design, add card delete and create logic

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,57 @@
-import React from 'react'
-import Span from '../../components/atoms/status/Status'
-import Tags from '../../components/atoms/tags/Tags'
-import HomeStyle from  './Home.module.css'
+import React, { useState } from "react";
+import Cards from "../../components/molecules/Cards/Cards";
+import CreateTodo from "../../components/organisms/Todo/createTodo/CreateTodo";
+import HomeStyle from "./Home.module.css";
 
 function Home() {
+  const [columns, setColumns] = useState([
+    {
+      title: "Todo",
+      tickets: [{ id: 1, priority: "Low", desc: "Wash the Pots" }],
+    },
+    {
+      title: "In Progress",
+      tickets: [{ id: 2, priority: "High", desc: "Complete exercises" }],
+    },
+    {
+      title: "Completed",
+      tickets: [{ id: 3, priority: "High", desc: "Complete exercises" }],
+    },
+  ]);
+
+  function onSave(ticket, columnIndex) {
+    const cols = [...columns];
+    cols[ticket.status].tickets.push(ticket);
+    setColumns(cols);
+  }
+
+  function onDelete(columnIndex, ticketIndex) {
+    const cols = [...columns];
+    cols[columnIndex].tickets.splice(ticketIndex, 1);
+    setColumns(cols);
+  }
+
   return (
     <div className={HomeStyle.Home}>
+      <CreateTodo onSave={onSave} />
+      {columns.map((column, columnIndex) => (
+        <div key={column.title}>
+          <h1>{column.title}</h1>
+          {column.tickets.map((ticket, ticketIndex, value) => (
+            <Cards
+              key={ticket.id}
+              description={ticket.desc}
+              priority={ticket.priority}
+              ticketTags={ticket.tags}
+              status={ticket.status}
+              displayDate={ticket.displayDate}
+              onDelete={() => onDelete(columnIndex, ticketIndex)}
+            />
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
